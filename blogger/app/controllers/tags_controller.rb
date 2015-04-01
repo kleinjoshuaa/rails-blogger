3732a7bcd6089467432f9d9b80a830f1257e684c
geogra4 <klein.joshua.a@gmail.com>
2015-03-31 22:31:55 -0400

    Changes to be committed: 	modified:   blogger/app/controllers/tags_controller.rb 	new file:   blogger/app/views/tags/index.html.erb 	modified:   blogger/app/views/tags/show.html.erb

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -2,5 +2,15 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
     end   
+    def index
+        @tags = Tag.all    
+    end
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "Tag '#{@tag.name}' Deleted!"
+        @tag.save
+        redirect_to root_path    
+    end
     
 end
